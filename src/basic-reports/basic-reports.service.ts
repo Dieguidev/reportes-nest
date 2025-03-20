0442bc1be1b3e6a39e50516672b92637165dc1ef
dieguidev <diegogaraycullas@gmail.com>
2025-03-19 22:23:48 -0500

    agregar soporte para generación de PDF en el módulo de informes básicos y actualizar la configuración de TypeScript

--- src/basic-reports/basic-reports.service.ts
+++ src/basic-reports/basic-reports.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 
+import PdfPrinter from 'pdfmake';
+import type { TDocumentDefinitions } from 'pdfmake/interfaces';
+
+const fonts = {
+  Roboto: {
+    normal: 'fonts/Roboto-Regular.ttf',
+    bold: 'fonts/Roboto-Medium.ttf',
+    italics: 'fonts/Roboto-Italic.ttf',
+    bolditalics: 'fonts/Roboto-MediumItalic.ttf',
+  },
+};
+
 @Injectable()
 export class BasicReportsService {
   constructor(private readonly prisma: PrismaService) {}
-  async hello() {
-    return await this.prisma.employees.findFirst();
+   hello() {
+    const printer = new PdfPrinter(fonts);
+    const docDefinition: TDocumentDefinitions = {
+      content: ['Hola mundo'],
+    };
+    const doc = printer.createPdfKitDocument(docDefinition);
+
+    return doc;
   }
 }
