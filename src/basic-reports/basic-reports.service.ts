908b9f784c81b807b6b549c474d40bf0c802045b
dieguidev <diegogaraycullas@gmail.com>
2025-03-20 22:49:30 -0500

    agregar funcionalidad para generar una carta de empleo en el módulo de informes básicos

--- src/basic-reports/basic-reports.service.ts
+++ src/basic-reports/basic-reports.service.ts
@@ -3,6 +3,7 @@ import type { TDocumentDefinitions } from 'pdfmake/interfaces';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 import { PrinterService } from 'src/printer/printer.service';
 import { getHelloWorldReport } from 'src/reports';
+import { getEmploymentLetterReport } from 'src/reports/employment-letter.report';
 
 @Injectable()
 export class BasicReportsService {
@@ -10,8 +11,17 @@ export class BasicReportsService {
     private readonly printerService: PrinterService,
     private readonly prisma: PrismaService,
   ) {}
+
   hello() {
     const docDefinition = getHelloWorldReport({ name: 'John Doe' });
+
+    const doc = this.printerService.createPdf(docDefinition);
+
+    return doc;
+  }
+
+  employmentLetter() {
+    const docDefinition = getEmploymentLetterReport();
 
     const doc = this.printerService.createPdf(docDefinition);
 
