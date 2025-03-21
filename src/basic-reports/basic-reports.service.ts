04fb43800a02c34eb7132b6667a893c09977a46f
dieguidev <diegogaraycullas@gmail.com>
2025-03-20 21:04:16 -0500

    agregar módulo Printer y servicio para la generación de PDFs, e integrar en el módulo de informes básicos

--- src/basic-reports/basic-reports.service.ts
+++ src/basic-reports/basic-reports.service.ts
@@ -1,27 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/database/prisma/prisma.service';
-
-import PdfPrinter from 'pdfmake';
 import type { TDocumentDefinitions } from 'pdfmake/interfaces';
-
-const fonts = {
-  Roboto: {
-    normal: 'fonts/Roboto-Regular.ttf',
-    bold: 'fonts/Roboto-Medium.ttf',
-    italics: 'fonts/Roboto-Italic.ttf',
-    bolditalics: 'fonts/Roboto-MediumItalic.ttf',
-  },
-};
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import { PrinterService } from 'src/printer/printer.service';
+import { getHelloWorldReport } from 'src/reports';
 
 @Injectable()
 export class BasicReportsService {
-  constructor(private readonly prisma: PrismaService) {}
-   hello() {
-    const printer = new PdfPrinter(fonts);
-    const docDefinition: TDocumentDefinitions = {
-      content: ['Hola mundo'],
-    };
-    const doc = printer.createPdfKitDocument(docDefinition);
+  constructor(
+    private readonly printerService: PrinterService,
+    private readonly prisma: PrismaService,
+  ) {}
+  hello() {
+    const docDefinition = getHelloWorldReport({ name: 'John Doe' });
+
+    const doc = this.printerService.createPdf(docDefinition);
 
     return doc;
   }
