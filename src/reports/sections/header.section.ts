8eebc44a6d4f288d43dd4401ae96ac1ef738025b
dieguidev <diegogaraycullas@gmail.com>
2025-03-22 22:05:39 -0500

    agregar fecha actual en el encabezado de los informes, ajustando el formato y la alineación

--- src/reports/sections/header.section.ts
+++ src/reports/sections/header.section.ts
@@ -9,6 +9,11 @@ const logo: Content = {
   margin: [0, 0, 0, 20],
 };
 
+const currentDate: Content = {
+  text: DateFormater.getDDMMMYYYY(new Date()),
+  alignment: 'right',
+}
+
 interface HeaderOptions {
   title?: string;
   subtitle?: string;
@@ -20,22 +25,49 @@ export const headerSection = (options: HeaderOptions): Content => {
   const { title, showDate = true, showLogo = true, subtitle } = options;
 
   const headerLogo: Content = showLogo ? logo : '';
-  const headerDate: Content = showDate
+  const headerDate: Content = showDate ? currentDate : '';
+
+  const headerSubtitle: Content = subtitle
     ? {
-        text: DateFormater.getDDMMMYYYY(new Date()),
-        alignment: 'right',
-        margin: [20, 20],
-      }
-    : '';
-  const headerTitle: Content = title
-    ? {
-        text: title,
+        text: subtitle,
+        alignment: 'center',
+        margin: [0, 2, 0, 0],
         style: {
+          fontSize: 16,
           bold: true,
         },
       }
     : '';
+
+  const headerTitle: Content = title
+    ? {
+        stack: [
+          {
+            text: title,
+            alignment: 'center',
+            margin: [0, 15, 0, 0],
+            style: {
+              bold: true,
+              fontSize: 22,
+            },
+          },
+          headerSubtitle,
+        ],
+        alignment: 'center',
+      }
+    : '';
   return {
-    columns: [headerLogo,headerTitle, headerDate],
+    columns: [
+      { width: 100, stack: [headerLogo] },
+      { width: '*', alignment: 'center', stack: [headerTitle] }, // Título en el centro con ancho adaptable
+      {
+        width: 150,
+        stack: [{
+          text: headerDate,
+          alignment: 'right',
+          margin: [0, 40, 20, 0] // Ajuste de margen para centrar verticalmente
+        }]
+      },
+    ],
   };
 };
