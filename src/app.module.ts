2af448c22649380983a80e768887cf27ddf9a364
dieguidev <diegogaraycullas@gmail.com>
2025-03-23 12:18:20 -0500

    agregar módulo y servicios para informes de tienda, incluyendo controlador y configuración de Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { BasicReportsModule } from './basic-reports/basic-reports.module';
 import { PrismaModule } from './database/prisma/prisma.module';
 import { PrinterModule } from './printer/printer.module';
+import { StoreReportsModule } from './store-reports/store-reports.module';
 
 @Module({
-  imports: [BasicReportsModule, PrismaModule, PrinterModule],
+  imports: [BasicReportsModule, PrismaModule, PrinterModule, StoreReportsModule],
 })
 export class AppModule {}
