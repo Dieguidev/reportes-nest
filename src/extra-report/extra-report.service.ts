6f88c5457ed123cc945653e01052d49c7a7aabd1
Dieguidev <diegogaraycullas@gmail.com>
2025-04-13 13:40:18 -0500

    feat: actualizar servicio de informes para incluir contenido dinámico y agregar pie de página en el PDF

--- src/extra-report/extra-report.service.ts
+++ src/extra-report/extra-report.service.ts
@@ -6,16 +6,19 @@ import { getHelloWorldReport } from 'src/reports';
 import { getHtmlContent } from 'src/helpers/html-to-pdfMake';
 import { TDocumentDefinitions } from 'pdfmake/interfaces';
 import { headerSection } from 'src/reports/sections/header.section';
+import { footerSection } from 'src/reports/sections/footer.section';
 
 @Injectable()
 export class ExtraReportService {
   constructor(private readonly printerService: PrinterService) {}
 
   getHtmlReport() {
-    const html = fs.readFileSync('src/reports/html/basi-01.html', 'utf-8');
-    console.log(html);
+    const html = fs.readFileSync('src/reports/html/basic-02.html', 'utf-8');
 
-    const content = getHtmlContent(html);
+    const content = getHtmlContent(html, {
+      client: 'Dieguidev',
+      title: 'Curso de Nestjs',
+    });
 
     const docDefinition: TDocumentDefinitions = {
       pageMargins: [40, 110, 40, 60],
@@ -23,6 +26,7 @@ export class ExtraReportService {
         title: 'HTML to PdfMake',
         subtitle: 'Convetir HTML a PDF',
       }),
+      footer: footerSection,
       content: content,
     };
 
--- src/helpers/html-to-pdfMake.ts
+++ src/helpers/html-to-pdfMake.ts
@@ -1,7 +1,22 @@
 import htmlToPdfmake from 'html-to-pdfmake';
 import { JSDOM } from 'jsdom';
 
-export const getHtmlContent = (html: string) => {
+interface ContentReplacement {
+  [key: string]: string;
+}
+
+//Record<string, string> is a type that represents an object with string keys and string values.
+
+export const getHtmlContent = (
+  html: string,
+  replacers: ContentReplacement = {},
+) => {
+  Object.entries(replacers).forEach(([key, value]) => {
+    const key1 = `{{${key}}}`;
+    const key2 = `{{${key}}}`;
+    html = html.replaceAll(key1, value).replaceAll(key2, value);
+  });
+
   const { window } = new JSDOM();
   return htmlToPdfmake(html, { window });
 };
