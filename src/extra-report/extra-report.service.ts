61f95880afab25a1a40f8bf5b619e33e20b538dc
Dieguidev <diegogaraycullas@gmail.com>
2025-04-13 12:47:38 -0500

    feat: agregar funcionalidad para generar informes HTML a PDF, incluyendo la lectura de archivos HTML y conversión a formato PDFMake

--- src/extra-report/extra-report.service.ts
+++ src/extra-report/extra-report.service.ts
@@ -1,13 +1,30 @@
+import fs from 'fs';
+
 import { Injectable } from '@nestjs/common';
 import { PrinterService } from 'src/printer/printer.service';
 import { getHelloWorldReport } from 'src/reports';
+import { getHtmlContent } from 'src/helpers/html-to-pdfMake';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
+import { headerSection } from 'src/reports/sections/header.section';
 
 @Injectable()
 export class ExtraReportService {
   constructor(private readonly printerService: PrinterService) {}
 
-  async getHtmlReport() {
-    const docDefinition = await getHelloWorldReport({ name: 'Hello World!' });
+  getHtmlReport() {
+    const html = fs.readFileSync('src/reports/html/basi-01.html', 'utf-8');
+    console.log(html);
+
+    const content = getHtmlContent(html);
+
+    const docDefinition: TDocumentDefinitions = {
+      pageMargins: [40, 110, 40, 60],
+      header: headerSection({
+        title: 'HTML to PdfMake',
+        subtitle: 'Convetir HTML a PDF',
+      }),
+      content: content,
+    };
 
     const doc = this.printerService.createPdf(docDefinition);
 
